4e5cde269d63439a826d0326ac325dc54ccfb547
HaDaHyeon <hada3817@gmail.com>
2024-08-27 19:39:08 +0900

    0827

--- solved.ac/1874.py
+++ solved.ac/1874.py
@@ -2,13 +2,13 @@ import sys
 input=sys.stdin.readline
 
 n=int(input())
-nums=[]
+nums=[] #수열
 for _ in range(n):
     nums.append(int(input()))
 
-stack=[0,1]
-count=1
-answer=['+']
+stack=[0]
+count=0 #스택에 삽입할 수 1~n
+answer=[]
 for i in range(n):   
     while stack[-1]<nums[i] or stack[-1]==0:
         count+=1
