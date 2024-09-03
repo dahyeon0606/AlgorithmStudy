dde1bee446fafcfa649d5e9c4bfeeb08d2e07dc9
HaDaHyeon <hada3817@gmail.com>
2024-09-03 16:26:58 +0900

    0903

---
+++ solved.ac/11659.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+nums=list(map(int,input().split()))
+for i in range(1,n): #O(100,000) 누적합 구하기
+    nums[i]+=nums[i-1]
+
+answer=[]
+
+for _ in range(m):
+    i,j=map(int,input().split())
+    if i==1:
+        answer.append(nums[j-1])
+    else:
+        answer.append(nums[j-1]-nums[i-2])  #3-1=2 1-2=-1
+
+for a in answer:
+    print(a)
